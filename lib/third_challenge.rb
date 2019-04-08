4f11f275068a1a8d7fd211d1d1035b48212a8195
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-08 21:29:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,18 +14,7 @@ def third_challenge
         age: "15",
         status: "alive"
         },
-      :hero_friends => [
-        {
-          name: "Benvolio",
-          age: "17",
-          attitude: "worried"
-        },
-        {
-          name: "Mercutio",
-          age: "18",
-          attitude: "hot-headed"
-        }
-        ]
+      :hero_friends => []
    },
    :capulet => {
       :patriarch => {
@@ -37,23 +26,11 @@ def third_challenge
         age: "51"
         },
       :heroine => {
-        {name: "Juliet",
+        name: "Juliet",
         age: "15",
         status: "alive"
         },
-      :heroine_friends => [
-        {
-          name: "Steven",
-          age: "30",
-          attitude: "confused"
-        },
-        {
-          name: "Nurse",
-          age: "44",
-          attitude: "worried"
-        }
-
-      ]
+      :heroine_friends => []
     }
   }
 end
