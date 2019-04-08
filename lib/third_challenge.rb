fad4bc219cb6bd1d13d5cd8eb86b5c206766f798
Kesha C Lake <keshalakemusic@gmail.com>
2019-04-08 20:49:10 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -10,7 +10,7 @@ def third_challenge
         #your key/value pairs here
         },
       :hero_friends => []
-   }, 
+   },
    :capulet => {
       :patriarch => {
         #your key/value pairs here
@@ -25,5 +25,5 @@ def third_challenge
    }
   }
 
-  
+
 end
